53a64a497c79209f2e59b3a12083c4cb8503a56e
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-09-09 16:40:42 +0900

    world의 add_player메서드에 주의사항 기재

--- crates/server/src/world.rs
+++ crates/server/src/world.rs
@@ -69,6 +69,10 @@ impl WorldInterface {
     }
 
     /// id가 겹치지 않음을 사용하는쪽에서 보장해야 함.
+    /// 보장하더라도 해싱된 결과가 같으면 충돌이 발생할 수 있음.
+    /// 1. mpsc를 사용해서 한 스레드에서만 add/remove를 수행하도록 한다.
+    /// 2. lockfree HashMap을 사용한다.
+    /// 3. 배열을 사용한다. (Vec<Option<Player>> 또는 [Option<Player>; MAX_PLAYER])
     pub async fn add_player(&self, id: u32) {
         self.as_mut().add_player(id);
     }
