98bfc802d1674735a8556004949003e92b5d7927
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-09-11 01:45:44 +0900

    World의 자료구조를 lockfree::map::Map으로 바꿔서 테스트

--- crates/server/src/session.rs
+++ crates/server/src/session.rs
@@ -29,7 +29,7 @@ impl Session {
     }
 
     pub async fn handle_connection(&mut self) {
-        self.world.add_player(self.id).await;
+        self.world.add_player(self.id);
 
         match self.stream_write(InitPacket::new(self.id, self.world.get_objects()).as_raw()).await {
             Ok(_) => {
@@ -57,7 +57,7 @@ impl Session {
             };
         }
 
-        self.world.remove_player(self.id).await;
+        self.world.remove_player(self.id);
     }
 
     
@@ -68,7 +68,7 @@ impl Session {
             match packet.packet_type() {
                 PacketType::PUSH => {
                     let push_packet = PushPacket::from_raw(packet);
-                    self.world.update_player(push_packet.player).await;
+                    self.world.update_player(push_packet.player);
 
                     let world = self.world.get_objects();
                     let raw_packet = PullPacket::new(world).as_raw();
@@ -84,7 +84,7 @@ impl Session {
                 }, 
                 PacketType::MOVE => {
                     let move_packet = MovePacket::from_raw(packet);
-                    self.world.move_player(self.id, move_packet.x, move_packet.y, move_packet.z).await;
+                    self.world.move_player(self.id, move_packet.x, move_packet.y, move_packet.z);
                 },
                 PacketType::MESSAGE => {
                     let message_packet = MessagePacket::from_raw(packet);
--- crates/server/src/world.rs
+++ crates/server/src/world.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use lockfree::map::Map;
 use mod_network::Player;     // 플레이어 프로토콜
 
 
@@ -7,13 +7,13 @@ pub type WorldPointer = usize;
 
 
 pub struct World {
-    players: HashMap<u32, Player>,
+    players: Map<u32, Player>,
 }
 
 impl World {
     pub fn new() -> Self {
         Self {
-            players: HashMap::new(),
+            players: Map::new(),
         }
     }
 
@@ -22,27 +22,26 @@ impl World {
         self.players.insert(id, Player { id, ..Default::default() });
     }
 
-    pub fn update_player(&mut self, player: Player) {
-        if let Some(old_player) = self.players.get_mut(&player.id) {
-            *old_player = player;
-        }
-    }
-
-    pub fn move_player(&mut self, id: u32, x: f32, y: f32, z: f32) {
-        if let Some(player) = self.players.get_mut(&id) {
-            player.translation.x += x;
-            player.translation.y += y;
-            player.translation.z += z;
-        }
-    }
-
     pub fn remove_player(&mut self, id: u32) {
         self.players.remove(&id);
     }
 
+    pub fn update_player(&mut self, player: Player) {
+        self.players.insert(player.id, player);
+    }
+
+    pub fn move_player(&mut self, id: u32, x: f32, y: f32, z: f32) {
+        let mut player = self.players.get(&id).unwrap().1.clone();
+        player.translation.x += x;
+        player.translation.y += y;
+        player.translation.z += z;
+        
+        self.update_player(player);
+    }
+
     pub fn get_objects(&self) -> Vec<Player> {
-        self.players.values()
-            .cloned()
+        self.players.iter()
+            .map(|player| player.1.clone())
             .collect()
     }
 }
@@ -64,29 +63,29 @@ pub struct WorldInterface {
 impl WorldInterface {
     pub fn new(world: WorldPointer) -> Self {
         Self { 
-            world: world as WorldPointer, 
+            world, 
         }
     }
 
     /// id가 겹치지 않음을 사용하는쪽에서 보장해야 함.
     /// 보장하더라도 해싱된 결과가 같으면 충돌이 발생할 수 있음.
-    /// 1. mpsc를 사용해서 한 스레드에서만 add/remove를 수행하도록 한다.
-    /// 2. lockfree HashMap을 사용한다.
-    /// 3. 배열을 사용한다. (Vec<Option<Player>> 또는 [Option<Player>; MAX_PLAYER])
-    pub async fn add_player(&self, id: u32) {
+    /// 1. mpsc를 사용해서 한 스레드에서만 add/remove를 수행하도록 한다.    >>>>>>> 자주 호출되지 않는 add/remove를 위해 task를 하나 할당해줘야함.
+    /// 2. lockfree HashMap을 사용한다.     >>>>>>> lockfree::map::Map으로 테스트, 성능확인 아직 안함.
+    /// 3. 배열을 사용한다. (Vec<Option<Player>> 또는 [Option<Player>; MAX_PLAYER])     >>>>>>> 오브젝트용 HashMap과 플레이어용 배열을 따로 관리해야한다.
+    pub fn add_player(&self, id: u32) {
         self.as_mut().add_player(id);
     }
 
-    pub async fn update_player(&self, player: Player) {
+    pub fn remove_player(&self, id: u32) {
+        self.as_mut().remove_player(id);
+    }
+
+    pub fn update_player(&self, player: Player) {
         self.as_mut().update_player(player);
     }
 
-    pub async fn move_player(&self, id: u32, x: f32, y: f32, z: f32) {
+    pub fn move_player(&self, id: u32, x: f32, y: f32, z: f32) {
         self.as_mut().move_player(id, x, y, z);
-    }
-
-    pub async fn remove_player(&self, id: u32) {
-        self.as_mut().remove_player(id);
     }
 
     pub fn get_objects(&self) -> Vec<Player> {
