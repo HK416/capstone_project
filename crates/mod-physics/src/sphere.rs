e0f1333d803127668f99cf37ae702d7d7ef454d8
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-09-24 01:08:37 +0900

    충돌체 Sphere 추가 - Sphere - Point - Sphere - Sphere

--- crates/mod-physics/src/sphere.rs
+++ crates/mod-physics/src/sphere.rs
@@ -2,3 +2,15 @@ pub struct Sphere {
     pub center: gmm::Float3,
     pub radius: f32,
 }
+
+impl Sphere {
+    pub fn check_point_collision(&self, point: &gmm::Float3) -> bool {
+        let p = gmm::Vector::from(*point - self.center);
+        p.vec3_len_sq() <= self.radius.powi(2)
+    }
+
+    pub fn check_sphere_collision(&self, sphere: &Sphere) -> bool {
+        let p = gmm::Vector::from(self.center - sphere.center);
+        p.vec3_len_sq() <= (self.radius + sphere.radius).powi(2)
+    }
+}
