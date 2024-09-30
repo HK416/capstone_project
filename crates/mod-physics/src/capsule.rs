c592ee910f0d5fed033677a5eef0453af484b54c
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-10-01 03:37:26 +0900

    Capsule의 direction을 단위벡터로 설정하기 위한 Capsule::build, 일부 함수 최적화 및 함수 설명 작성

--- crates/mod-physics/src/capsule.rs
+++ crates/mod-physics/src/capsule.rs
@@ -2,19 +2,35 @@ use super::Sphere;
 use mod_math::{Segment, Line};
 
 
-/// height <= 2 * radius 인 경우, Capsule-Capsule 충돌은 제대로 동작하지 않는다.  
+/// height 및 radius가 음수인 경우는 고려하지 않는다.  
+/// direction은 단위벡터이다.  
+/// height == 2 * radius이면 캡슐은 구와 같은 모양이 되는데, 구의 충돌체크로 최적화 하지는 않는다.  
+/// height <= 2 * radius이면 캡슐에 기둥이 없고 위아래가 둥근 UFO형태가 되는데, 
+/// 이때 일부 함수가 제대로 동작하지 않을 수 있다.  
 #[derive(Debug)]
 pub struct Capsule {
-    pub center: gmm::Float3,     // 캡슐의 가장 아래 부분
-    pub direction: gmm::Float3,  // 캡슐의 윗부분이 향하는 방향(default: Y축)
-    pub height: f32,
+    pub center: gmm::Float3,    // 캡슐의 가장 아래 부분
+    direction: gmm::Float3,     // 캡슐의 윗부분이 향하는 방향(default: Y축)
+    pub height: f32,            // 캡슐의 전체 높이(direction방향으로 뻗은 길이) 
     pub radius: f32,
 }
 
 impl Capsule {
+    /// direction을 단위벡터로 만들어 Capsule을 생성한다.  
+    /// 영벡터가 주어지면 Error를 반환한다.  
+    pub fn build(center: gmm::Float3, direction: gmm::Float3, height: f32, radius: f32) -> Result<Self, &'static str> {
+        match gmm::Vector::from(direction).vec3_normalize() {
+            Some(direction) => Ok(Self { 
+                center, direction: direction.into(), height, radius 
+            }),
+            None => Err("Direction cannot be zero vector")
+        }
+    }
+    
+    /// 캡슐이 UFO형태인 경우에도 제대로 동작한다.
     pub fn check_point_collision(&self, point: &gmm::Float3) -> bool {
         let p = gmm::Vector::from(*point - self.center);    // center에 대한 point의 상대좌표
-        let d = self.direction_normal();
+        let d = gmm::Vector::from(self.direction);
 
         let dot: gmm::Float3 = d.vec3_dot(p).into();
         // 캡슐에 대한 point의 상대지역좌표 y
@@ -24,13 +40,13 @@ impl Capsule {
         }
 
         // 캡슐에 대한 point의 상대지역좌표 x(원통의 회전은 신경쓰지 않아도 되므로 z무시)
-        let x2 = p.vec3_len_sq() - y * y;
-        if x2 > self.radius.powi(2) {
+        let x_sq = p.vec3_len_sq() - y * y;
+        let radius_sq = self.radius.powi(2);
+        if x_sq > radius_sq {
             return false;
         }
 
-        let h2 = self.radius * self.radius - x2;
-        let h = h2.sqrt();
+        let h = (radius_sq - x_sq).sqrt();
         let top_y = self.height - self.radius   + h;
         let bot_y = self.radius                 - h;
 
@@ -38,12 +54,12 @@ impl Capsule {
         bot_y <= y && y <= top_y
     }
 
+    /// 캡슐이 UFO형태인 경우에도 제대로 동작한다.
+    /// 
     /// Capsule의 크기를 sphere의 radius만큼 확장하고 sphere의 중심점과 충돌하는지 체크
     pub fn check_sphere_collision(&self, sphere: &Sphere) -> bool {
-        let d: gmm::Float3 = self.direction_normal().into();
-
         let capsule = Capsule {
-            center: self.center - d * sphere.radius,
+            center: self.center - self.direction * sphere.radius,
             direction: self.direction,
             height: self.height + 2.0 * sphere.radius,
             radius: self.radius + sphere.radius,
@@ -52,6 +68,8 @@ impl Capsule {
         capsule.check_point_collision(&sphere.center)
     }
 
+    /// 캡슐이 UFO형태인 경우에는 제대로 동작하지 않는다.
+    /// 
     /// 두 선분 사이의 거리를 구하는 것과 같은가?
     /// 캡슐은 한 선분에서 radius거리 이내의 모든 점의 집합  
     /// 이는 self를 sphere.radius만큼 확장한 캡슐과 나머지 캡슐의 양쪽 구의 중심을 이은 선분이 충돌하는지 체크하는것과 같다.
@@ -68,20 +86,11 @@ impl Capsule {
         distance <= self.radius + other.radius
     }
 
-    pub fn direction_normal(&self) -> gmm::Vector {
-        match gmm::Vector::from(self.direction).vec3_normalize() {
-            Some(v) => v,
-            None => gmm::Vector::from(gmm::Float3::Y),            // default: Y축
-        }
-    }
-
     /// 캡슐의 아래 구의 중심과 윗 구의 중심을 이은 선분
     fn get_seg(&self) -> Segment {
-        let d: gmm::Float3 = self.direction_normal().into();
-
         Segment {
-            start: self.center + d * self.radius,
-            end: self.center + d * (self.height - self.radius),
+            start: self.center + self.direction * self.radius,
+            end: self.center + self.direction * (self.height - self.radius),
         }
     }
 }
@@ -90,14 +99,19 @@ impl Capsule {
 
 /// Y축에 정렬된 캡슐  
 /// direction은 항상 <0, 1, 0>이다.  
+/// height 및 radius가 음수인 경우는 고려하지 않는다.  
+/// height == 2 * radius이면 캡슐은 구와 같은 모양이 되는데, 구의 충돌체크로 최적화 하지는 않는다.  
+/// height <= 2 * radius이면 캡슐에 기둥이 없고 위아래가 둥근 UFO형태가 되는데, 
+/// 이때 일부 함수가 제대로 동작하지 않을 수 있다.  
 #[derive(Debug)]
 pub struct YCapsule {
-    pub center: gmm::Float3,     // 캡슐의 가장 아래 부분
-    pub height: f32,
+    pub center: gmm::Float3,    // 캡슐의 가장 아래 부분
+    pub height: f32,            // 캡슐의 전체 높이
     pub radius: f32,
 }
 
 impl YCapsule {
+    /// 캡슐이 UFO형태인 경우에도 제대로 동작한다.  
     pub fn check_point_collision(&self, point: &gmm::Float3) -> bool {
         match self.get_y_range_at(point.x, point.z) {
             Some((bot_y, top_y)) => bot_y <= point.y && point.y <= top_y,
@@ -105,7 +119,7 @@ impl YCapsule {
         }
     }
 
-    /// 이 함수는 height <= 2 * radius인 경우에는 제대로 동작하지 않는다.  
+    /// 캡슐이 UFO형태인 경우에는 제대로 동작하지 않는다.  
     /// 
     /// #### y축과 평행한 경우
     /// 캡슐과 직선을 캡슐의 center가 원점이 되도록 평행이동 시킨 후, 
@@ -158,6 +172,8 @@ impl YCapsule {
         false
     }
 
+    /// 캡슐이 UFO형태인 경우에도 제대로 동작한다.  
+    /// 
     /// Capsule의 크기를 sphere의 radius만큼 확장하고 sphere의 중심점과 충돌하는지 체크
     pub fn check_sphere_collision(&self, sphere: &Sphere) -> bool {
         let mut center = self.center.clone();
@@ -172,26 +188,36 @@ impl YCapsule {
         capsule.check_point_collision(&sphere.center)
     }
 
+    /// other캡슐이 UFO형태인 경우에는 제대로 동작하지 않는다.
+    /// 
     /// Y축에 정렬된 두 캡슐의 충돌 체크
     pub fn check_ycapsule_collision(&self, other: &YCapsule) -> bool {
+        let mut center = self.center.clone();
+        center.y -= other.radius;
+
         let capsule = YCapsule {
-            center: self.center,
+            center,
             height: self.height + 2.0 * other.radius,
             radius: self.radius + other.radius,
         };
 
         match capsule.get_y_range_at(other.center.x, other.center.z) {
             Some((bot_y, top_y)) => {
-                let other_bot_y = other.center.y;
-                let other_top_y = other.center.y + other.height;
+                let Segment { start, end } = other.get_seg();
 
-                bot_y <= other_bot_y && other_bot_y <= top_y ||
-                bot_y <= other_top_y && other_top_y <= top_y
+                if end.y < bot_y || top_y < start.y {
+                    return false;
+                }
+                else {
+                    return true;
+                }
             },
             None => false,
         }
     }
 
+    /// 캡슐이 UFO형태인 경우에 bottom > top이 되는 구간은 포함하지 않는다.  
+    /// 
     /// x, z에서 캡슐의 y축 범위를 구하고(bottom, top)형태로 반환한다.  
     /// x, z가 캡슐의 외부에 있으면 None을 반환한다.
     fn get_y_range_at(&self, x: f32, z: f32) -> Option<(f32, f32)> {
@@ -213,6 +239,8 @@ impl YCapsule {
         Some((bot_y, top_y))
     }
 
+    /// 캡슐이 UFO형태인 경우는 고려하지 않는다.  
+    /// 
     /// 캡슐의 아래 구의 중심과 윗 구의 중심을 이은 선분
     fn get_seg(&self) -> Segment {
         let mut start = self.center.clone();
