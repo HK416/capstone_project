a996b13a470bf0036a6893975aa031576ed26691
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-10-07 02:54:20 +0900

    Revert "Merge branch 'server' into feature_28"
    This reverts commit af2b9f116f882a5abdb2410caa539991bad3702e, reversing
    changes made to ad982a50200a0c1ce731fa846fecdff7d02d2c33.

--- crates/mod-physics/src/lib.rs
+++ crates/mod-physics/src/lib.rs
@@ -2,12 +2,3 @@ pub mod rigid_body;
 mod bounds;
 
 pub use self::bounds::*;
-
-mod sphere;
-pub use self::sphere::*;
-
-mod capsule;
-pub use self::capsule::*;
-
-mod ray;
-pub use self::ray::*;
--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -3,15 +3,11 @@ use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
 };
 use std::sync::Mutex;
-use std::env;
-use std::str::FromStr;
 
 use server::{
     world::*,
     session::Session,
 };
-
-use mod_network::Addr;
 
 
 
@@ -32,7 +28,7 @@ pub async fn run_server(addr: &str) {
 }
 
 
-const MAX_CLIENTS: usize =  1000;
+const MAX_CLIENTS: usize =  10;
 /// World를 직접 읽으면 최신 데이터가 아닐 가능성이 있다.  
 /// World에 Mutex, RwLock등을 걸면 클라이언트가 읽는데 병목이 생길 수 있다.  
 /// 따라서 클라이언트 개수만 세기 위해 따로 분리.  
@@ -97,21 +93,7 @@ async fn server_full(mut stream: TcpStream) {
 
 #[tokio::main]
 async fn main() {
-    let mut args = env::args();
-    args.next();
-
-    let addr = match args.next() {
-        Some(args) => match Addr::from_str(&args) {
-            Ok(addr) => addr,
-            Err(e) => {
-                eprintln!("{}", e);
-                return;
-            }
-        },
-        None => Addr::default()
-    };
-
-    run_server(&addr.to_string()).await;
+    run_server("localhost:7878").await;
 }
 
 
