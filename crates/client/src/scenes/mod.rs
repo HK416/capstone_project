e71dcc6eec43ae5a599b7b3564e869be00056fa3
HK416 <powerspirit127@gmail.com>
2025-03-09 11:20:41 +0900

    커스텀 게임 대기실 장면 초안 추가

--- crates/client/src/scenes/mod.rs
+++ crates/client/src/scenes/mod.rs
@@ -1,5 +1,26 @@
 mod intro;
+mod room;
 mod startup;
 mod testbed;
 
-pub use {self::intro::*, self::startup::*, self::testbed::*};
+use std::fmt;
+
+pub use self::{intro::*, room::*, startup::*, testbed::*};
+
+impl fmt::Debug for CustomGameEnterScene {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", stringify!(CustomGameEnterScene))
+    }
+}
+
+impl fmt::Debug for CustomGameExitScene {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", stringify!(CustomGameExitScene))
+    }
+}
+
+impl fmt::Debug for CustomGameRoomScene {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", stringify!(CustomGameRoomScene))
+    }
+}
