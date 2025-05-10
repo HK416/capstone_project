39eb1669d282ad06f473a0a9b043950f32cebef8
HK416 <powerspirit127@gmail.com>
2025-05-10 16:54:24 +0900

    캐릭터 카메라 위치 변경 - 3인칭 시점으로 수정

--- crates/client/src/component/character/momoi_original.rs
+++ crates/client/src/component/character/momoi_original.rs
@@ -59,11 +59,11 @@ pub const VICTORY_END_DURATION: f32 = 1.667;
 /// 캐릭터 모델의 카메라 기본 위치입니다.
 pub const CAMERA_IDLE_POSITION: glam::Vec3A = glam::vec3a(0.25, 0.85, 1.5);
 /// 캐릭터 모델의 카메라 줌 위치입니다.
-pub const CAMERA_ZOOM_POSITION: glam::Vec3A = glam::vec3a(0.125, 0.7, 0.5);
+pub const CAMERA_ZOOM_POSITION: glam::Vec3A = glam::vec3a(0.25, 0.7, 0.15);
 /// 캐릭터 모델의 카메라 기본 Fov-y 라디안 각도입니다.
-pub const CAMERA_IDLE_FOV_Y: f32 = 1.309; // 75도
+pub const CAMERA_IDLE_FOV_Y: f32 = 75f32.to_radians();
 /// 캐릭터 모델의 카메라 줌 Fov-y 라디안 각도 입니다.
-pub const CAMERA_ZOOM_FOV_Y: f32 = 1.22173; // 70도
+pub const CAMERA_ZOOM_FOV_Y: f32 = 65f32.to_radians();
 
 /// `Bip001_Head`의 `*_Normal_Attack_Ing` 애니메이션 첫 번째 키 프레임에서 월드 좌표계 X축을 로컬 좌표계로 변환한 벡터입니다.
 pub const HEAD_W2L_X_NORMAL_ATTACK_ING: glam::Vec3 = glam::vec3(-0.0489224, 0.6057397, -0.7941568);
