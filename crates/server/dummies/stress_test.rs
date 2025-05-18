abde1d235cd9895a4941f734822e2fca93e2a342
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2025-05-19 02:58:45 +0900

    게임 시작이 성공할때까지 ready packet 반복전송 - 실패시 200ms sleep

--- crates/server/dummies/stress_test.rs
+++ crates/server/dummies/stress_test.rs
@@ -1,14 +1,21 @@
-use mod_network::components::{Email, LoginToken, Passwd, UserAccount, WorldId};
 use rand::seq::IndexedRandom;
-use tokio::net::TcpStream;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::{
+    net::TcpStream,
+    io::{AsyncReadExt, AsyncWriteExt},
+};
 
-use mod_network::protocol::{
-    CustomGameJoinRequestPacket, CustomGameJoinSuccessPacket, 
-    CustomGamePullPacket, CustomGameReadyPacket, 
-    LoginRequestPacket, LoginSuccessPacket, 
-    Packet, PacketParser, PacketType, 
-    AvailableWorldsPacket, RequestAvailableWorldsPacket
+use mod_network::{
+    components::{
+        Email, LoginToken, Passwd, Permission, SelectResult, UserAccount, WorldId
+    },
+    protocol::{
+        Packet, PacketParser, PacketType, 
+        LoginRequestPacket, LoginSuccessPacket, 
+        CustomGameJoinRequestPacket, CustomGameJoinSuccessPacket, 
+        AvailableWorldsPacket, RequestAvailableWorldsPacket, 
+        CustomGamePullPacket, CustomGameReadyPacket, CustomGameStartFailedPacket,
+        FormationPullPacket, FormationSelectResponsePacket,
+    }
 };
 
 
@@ -53,11 +60,9 @@ impl Client {
             let available = self.request_available_worlds().await?;
 
             let world_id = if available.is_empty() {
-                println!("No available worlds, creating a new one...");
                 // 방 생성
                 WorldId::NULL
             } else {
-                println!("Available worlds: {:?}", available);
                 // 랜덤으로 방 선택
                 *available.choose(&mut rand::rng()).unwrap()
             };
@@ -73,6 +78,9 @@ impl Client {
 
         // 준비 신호 전송
         self.ready().await?;
+
+        // 캐릭터 선택
+        self.select_character().await?;
 
         tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
 
@@ -90,7 +98,6 @@ impl Client {
                 ));
             }
             Ok(n) => {
-                // println!("Received {} bytes: {:?}", n, &buf[..n]);
                 self.packet_parser.push(&buf[..n]);
                 return Ok(());
             }
@@ -109,12 +116,10 @@ impl Client {
             self.read().await?;
 
             while let Some(packet) = self.packet_parser.pop() {
-                // println!("Parsed packet: {:?}", packet);
                 if packet.packet_type() == PacketType::LoginSuccess {
                     let p = LoginSuccessPacket::from_raw(packet);
                     self.account = p.account;
                     self.token = p.token;
-                    // println!("Login success: {:?}", p);
                     break 'readloop;
                 }
             }
@@ -131,7 +136,6 @@ impl Client {
             self.read().await?;
 
             while let Some(packet) = self.packet_parser.pop() {
-                // println!("Parsed packet: {:?}", packet);
                 if packet.packet_type() == PacketType::AvailableWorlds {
                     let p = AvailableWorldsPacket::from_raw(packet);
                     return Ok(p.worlds);
@@ -148,10 +152,8 @@ impl Client {
             self.read().await?;
 
             while let Some(packet) = self.packet_parser.pop() {
-                // println!("Parsed packet: {:?}", packet);
                 if packet.packet_type() == PacketType::CustomGameJoinSuccess {
-                    let p = CustomGameJoinSuccessPacket::from_raw(packet);
-                    // println!("Join success: {:?}", p);
+                    let _p = CustomGameJoinSuccessPacket::from_raw(packet);
                     break 'readloop;
                 }
             }
@@ -161,18 +163,68 @@ impl Client {
     }
 
     async fn ready(&mut self) -> Result<(), std::io::Error> {
-        let packet = CustomGameReadyPacket::new(self.account.uid, self.token, true).as_raw();
+        let ready_packet = CustomGameReadyPacket::new(self.account.uid, self.token, true).as_raw();
+
+        'writeloop: loop {
+            self.writer.write_all(&ready_packet.as_bytes()).await?;
+
+            'readloop: loop {
+                self.read().await?;
+
+                while let Some(packet) = self.packet_parser.pop() {
+                    match packet.packet_type() {
+                        // 플레이어들의 상태에 업데이트가 있다면 
+                        PacketType::CustomGamePull => {
+                            let p = CustomGamePullPacket::from_raw(packet);
+
+                            if p.players.iter()
+                                .find(|p| p.account.uid == self.account.uid)
+                                .filter(|p| p.permission() == Permission::Admin)
+                                .is_some() {
+                                // 방장이면 게임 시작 신호(ready packet) 재전송
+                                continue 'writeloop;
+                            } else {
+                                // 방장이 아니면 게임 시작 신호 대기
+                                continue 'readloop;
+                            }
+                        }
+                        // 이 메세지를 받는다면 방장임
+                        PacketType::CustomGameStartFailed => {
+                            let p = CustomGameStartFailedPacket::from_raw(packet);
+                            println!("Game start failed: {:?}", p.reason);
+                            tokio::time::sleep(tokio::time::Duration::from_millis(200)).await;
+                            continue 'writeloop;
+                        }
+                        // 이 메세지를 받는다면 게임이 시작된것임
+                        PacketType::FormationPull => {
+                            let _p = FormationPullPacket::from_raw(packet);
+                            println!("Game started");
+                            break 'writeloop;
+                        }
+                        _ => {
+                            println!("Invalid packet received: {:?}", packet.packet_type());
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn select_character(&mut self) -> Result<(), std::io::Error> {
+        // 캐릭터 선택 패킷 전송
+        let packet = FormationSelectResponsePacket::new(SelectResult::Success).as_raw();
         self.writer.write_all(&packet.as_bytes()).await?;
 
-        'readloop: loop {
+        // 캐릭터 선택 완료 대기
+        loop {
             self.read().await?;
 
             while let Some(packet) = self.packet_parser.pop() {
-                // println!("Parsed packet: {:?}", packet);
-                if packet.packet_type() == PacketType::CustomGamePull {
-                    let p = CustomGamePullPacket::from_raw(packet);
-                    // println!("Pull: {:?}", p);
-                    break 'readloop;
+                if packet.packet_type() == PacketType::FormationPull {
+                    let _p = FormationPullPacket::from_raw(packet);
+                    break;
                 }
             }
         }
@@ -186,12 +238,14 @@ impl Client {
 async fn main() -> Result<(), std::io::Error> {
     println!("Stress test started");
 
-    loop {
+    const MAX_CLIENTS: usize = 1000;
+
+    for _ in 0..MAX_CLIENTS {
         let mut client = Client::new(UserAccount::default()).await.unwrap();
         tokio::spawn(async move { client.run().await });
 
-        // 접속속도 초당 10개로 제한
-        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+        // 접속속도 초당 50개로 제한
+        tokio::time::sleep(tokio::time::Duration::from_millis(20)).await;
     }
 
     println!("Stress test finished");
